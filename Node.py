7f54a9b6ebc8b654c33c3382ebeda5c81f1da2e2
Lakshay <lakshaysharmadav1@gmail.com>
2024-03-15 02:04:17 +0530

    Next Commit

--- Node.py
+++ Node.py
@@ -2,7 +2,9 @@ from Blockchain import Blockchain
 from TransactionPool  import TransactionPool
 from Wallet import Wallet
 from SocketCommunication import SocketCommunication
-
+from NodeAPI import NodeAPI
+from Message import Message
+from BlockchainUtils import BlockhainUtils
 class Node():
 
     def __init__(self, ip, port):
@@ -15,5 +17,23 @@ class Node():
 
     def startP2P(self):
         self.p2p = SocketCommunication(self.ip, self.port)
-        self.p2p.startSocketCommunication()
+        self.p2p.startSocketCommunication(self)
+
+    def startAPI(self, apiPort):
+        self.api = NodeAPI()
+        self.api.injectNode(self)
+        self.api.start(apiPort)
+    
+    def handleTransaction(self, transaction):
+        data = transaction.payload()
+        signature = transaction.signature
+        signerPublicKey = transaction.senderPublicKey
+        signatureValid = Wallet.signatureValid(data, signature, signerPublicKey)
+        transactionExists = self.transactionPool.transactionExists(transaction)
+        if not transactionExists and signatureValid:
+            self.transactionPool.addTransaction(transaction)
+            message = Message(self.p2p.socketConnector, 'TRANSACTION', transaction)
+            encodedMessage = BlockhainUtils.encode(message)
+            self.p2p.broadcast(encodedMessage)
+
 
